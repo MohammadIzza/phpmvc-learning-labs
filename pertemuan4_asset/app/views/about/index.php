b1ca063bbfd540a6a566a34c7c8285e9a3909cf8
MOHAMMAD IZZA HAKIKI <m.izza.hakiki@gmail.com>
2026-05-19 18:04:17 +0700

    Session 4 (assets and public resources): view: about/index.php

---
+++ pertemuan4_asset/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container bg-success">
+    <h1> Ini adalah about me </h1>
+    <p>Halo nama saya <?= $data['nama']; ?>. saya seorang <?= $data['pekerjaan']; ?> dan saat ini saya berumur <?= $data['umur']; ?> </p>
+    <img src="<?= BASEURL ?>/img/gambar.jpg" width="500" alt="gambar">
+</div>
